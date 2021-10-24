2a18772a56714eb7539ceb96a06b77a72c7c53f0
victoray <viktoray@007@gmail.com>
2021-10-24 13:24:27 +0300

    add role

--- ecare/role/models.py
+++ ecare/role/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Role(models.Model):
+    PATIENT = "patient"
+    PROVIDER = "provider"
+
+    TYPE_CHOICES = [
+        (PATIENT, "Patient"),
+        (PROVIDER, "Provider"),
+    ]
+    name = models.CharField()
+    type = models.CharField(choices=TYPE_CHOICES)
+
+    def is_patient(self):
+        return self.type == self.PATIENT
+
+    def is_provider(self):
+        return self.type == self.PROVIDER
