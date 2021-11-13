ba4822c98c6c8241e16bf07b5c44ca72d04a1888
victoray <viktoray@007@gmail.com>
2021-11-13 21:57:24 +0200

    firebase auth

--- ecare/settings.py
+++ ecare/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "drf_firebase_auth",
     # app
     "ecare.user",
     "ecare.role",
@@ -130,3 +136,11 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "user.User"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "drf_firebase_auth.authentication.FirebaseAuthentication",
+    ]
+}
