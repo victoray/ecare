495e12bea01162e9edc3a7a4773f12faea16e8d2
victoray <viktoray@007@gmail.com>
2021-11-28 21:18:21 +0200

    fix typo

--- ecare/settings.py
+++ ecare/settings.py
@@ -162,3 +162,10 @@ TWILIO_CHAT_SERVICE_SID = os.environ.get("TWILIO_CHAT_SERVICE_SID", None)
 
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
