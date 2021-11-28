760f8b544f701746e7c9ae2b1e98127b7ff2c21e
victoray <viktoray@007@gmail.com>
2021-11-28 22:43:02 +0200

    staticfiles

--- ecare/settings.py
+++ ecare/settings.py
@@ -165,7 +165,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
 
 # # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
